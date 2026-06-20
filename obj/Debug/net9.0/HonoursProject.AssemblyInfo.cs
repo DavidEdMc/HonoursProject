0505eb4a66f2bcac66c01d96de0dba8d7d9d055b
David McIntyre <DavidM@forrit.com>
2026-06-20 14:21:10 +0100

    adding git.ignore

--- obj/Debug/net9.0/HonoursProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/HonoursProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HonoursProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51f8fcac5fb766bcc50bda76f3e1c441678551d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0204fcd28096de30f4affe0e2202a9467873ad33")]
 [assembly: System.Reflection.AssemblyProductAttribute("HonoursProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HonoursProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
